8604277e6caed294e3e9115550513df5c67650b8
Denis Panysh <71315618+Punisher2407@users.noreply.github.com>
2021-03-21 17:40:41 +0300

    Revert "Revert "Encryption by the "Railway fence" method.""
    This reverts commit 73a48f928d66443e9241a7ddf5a901ba8e734172.

--- basic_cryptography/basic_cryptography/Program.cs
+++ basic_cryptography/basic_cryptography/Program.cs
@@ -4,12 +4,72 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace basic_cryptography
 {
-    class Program
+    public class Permutation
     {
-        static void Main()
+        public string source_text = "";
+        public int key = 0;
+        public string EncryptRailwayFence(int Key, string SourceText)
         {
+            bool goDown = false;
+            int row = 0, column = 0;
+            int i = 0, j = 0;
+            string ciphertext = "";
+            char[,] Fence = new char[Key, SourceText.Length];
+
+            // Инициализация массива, символом, котрый не используется в тексте.
+            for (i = 0; i < Key; i++)
+            {
+                for (j = 0; j < SourceText.Length; j++)
+                {
+                    Fence[i, j] = '~';
+                }
+            }
+
+            for (i = 0; i < SourceText.Length; i++)
+            {
+                if ((row == 0) || (row == Key - 1))
+                {
+                    goDown = !goDown;
+                }
+                Fence[row, column] = SourceText[i];
+                column++;
+                if (goDown)
+                {
+                    row++;
+                }
+                else
+                {
+                    row--;
+                }
+            }
+
+            for (i = 0; i < Key; i++)
+            {
+                for (j = 0; j < SourceText.Length; j++)
+                {
+                    if (Fence[i, j] != '~')
+                    {
+                        ciphertext += Fence[i, j];
+                    }
+                }
+            }
+            return ciphertext;
         }
     }
+    public class Program
+    { 
+        static void Main()
+        {
+            // test - 1, Telecommunication
+            Permutation RailwayFence = new Permutation();
+            Console.Write("Source text: ");
+            RailwayFence.source_text = Console.ReadLine();
+            Console.Write("Input key: ");
+            RailwayFence.key = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ciphertext: {0}", RailwayFence.EncryptRailwayFence(RailwayFence.key, RailwayFence.source_text));
+        }
+    }    
 }
